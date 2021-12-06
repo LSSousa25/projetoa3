87843f927e0dff8ca2ccf6e4da07024b8073658c
LSSousa25 <70540464+LSSousa25@users.noreply.github.com>
2021-12-06 19:08:33 -0300

    0.1

---
+++ src/test/java/com/sistemasdistribuidos/a3/projeto_a3/ProjetoA3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasdistribuidos.a3.projeto_a3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoA3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
